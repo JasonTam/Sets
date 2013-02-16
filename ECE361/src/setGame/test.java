881ad4e6a9d59158ad15c630fc49e9fe286fd9e5
Jason Tam <JasonTam22@gmail.com>
2013-02-16 15:49:56 -0500

    The game (textbased in console) is now 1 person playable via setGame/test. Far from elegant/robust.

--- ECE361/src/setGame/test.java
+++ ECE361/src/setGame/test.java
@@ -11,6 +11,7 @@ public class test {
         Card a = new Card(65);
         Card b = new Card(66);
         Card c = new Card(70);
+
         System.out.println("Card a: " + a.toString());
         System.out.println("Card b: " + b.toString());
         System.out.println("Card c: " + c.toString());
@@ -21,7 +22,7 @@ public class test {
         setfound = game1.isSet(a, b, c);
         System.out.println("a,b,c form a set? " + setfound);
         
-//        game1.init();
+        game1.playGame();
         
     }
 	
