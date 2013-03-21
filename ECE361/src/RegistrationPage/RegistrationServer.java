3d21f1a2b58a3dc61f7aee6d0d3b37512a1b5b3f
Sophomore <perr@cooper.edu>
2013-03-20 20:46:25 -0400

    i hate egit

--- ECE361/src/RegistrationPage/RegistrationServer.java
+++ ECE361/src/RegistrationPage/RegistrationServer.java
@@ -3,17 +3,27 @@
 package RegistrationPage;
 
 import javax.servlet.http.HttpServletRequest;
+
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
  
+import java.io.FileInputStream;
 import java.io.IOException;
  
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
- 
+
+import java.io.File;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+
+
 public class RegistrationServer extends AbstractHandler
 {
+	static String signup_template;
+	
     public void handle(String target,
                        Request baseRequest,
                        HttpServletRequest request,
@@ -23,15 +33,34 @@ public class RegistrationServer extends AbstractHandler
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
-        response.getWriter().println("<h1>Hello World</h1>");
+        response.getWriter().println(signup_template);
     }
  
     public static void main(String[] args) throws Exception
     {
+    	
+    	// read templates into memory
+    	RegistrationServer.signup_template = readFile("src/RegistrationPage/templates/signup.html");
+    	
+    	
         Server server = new Server(8080);
         server.setHandler(new RegistrationServer());
  
         server.start();
         server.join();
     }
+    
+    /* recipe from stackoverflow */
+    private static String readFile(String path) throws IOException {
+    	  FileInputStream stream = new FileInputStream(new File(path));
+    	  try {
+    	    FileChannel fc = stream.getChannel();
+    	    MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+    	    /* Instead of using default, pass in a decoder. */
+    	    return Charset.defaultCharset().decode(bb).toString();
+    	  }
+    	  finally {
+    	    stream.close();
+    	  }
+    	}
 }
