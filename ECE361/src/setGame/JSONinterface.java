b544d27aa9d0c99a6a604b774f5ec318036c60bf
Jason Tam <JasonTam22@gmail.com>
2013-03-24 19:29:45 -0400

    gson for submitting sets and receiving actions.

--- ECE361/src/setGame/JSONinterface.java
+++ ECE361/src/setGame/JSONinterface.java
@@ -1,5 +1,8 @@
 package setGame;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import com.google.gson.*;
 
 public class JSONinterface {
@@ -7,4 +10,30 @@ public class JSONinterface {
 	public static void getJSON(Game game) {
 		
 	}
+	
+	public static String colToJson (String action, Collection<Card> set) {
+		Collection col = new ArrayList();
+		col.add(action);
+		col.add(set);
+		Gson gson = new Gson();
+		return gson.toJson(col);
+	}
+	
+	public static String jsonGetAction (String json) {
+		JsonParser parser = new JsonParser();
+	    JsonArray array = parser.parse(json).getAsJsonArray();
+	    Gson gson = new Gson();
+	    String action = gson.fromJson(array.get(0), String.class);
+	    return action;
+	}
+	
+	public static Collection jsonGetData (String json) {
+		JsonParser parser = new JsonParser();
+	    JsonArray array = parser.parse(json).getAsJsonArray();
+	    Gson gson = new Gson();
+	    Collection data = (ArrayList<Card>)gson.fromJson(array.get(1), ArrayList.class);
+		return data;
+	}
+	
+    
 }
