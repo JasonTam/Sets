2c08b8d2237d6391fe30490165c25a49fdbd04cf
nock-wong <nick@radiatorlabs.com>
2013-03-30 18:27:36 -0400

    Edit submitSet return values.

--- ECE361/src/setGame/Field.java
+++ ECE361/src/setGame/Field.java
@@ -1,7 +1,6 @@
 package setGame;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Field {
 	
--- ECE361/src/setGame/GameLogic.java
+++ ECE361/src/setGame/GameLogic.java
@@ -1,8 +1,6 @@
 package setGame;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 public class GameLogic {
 
